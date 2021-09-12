bfcd93fa783bb7eb5fdf7245acab25665af5bff3
Gion Desax <gion.desax@script-source.net>
2021-09-12 22:16:28 +0200

    Update Date Calculator

--- src/net/scriptsource/aufgaben/Aufgabe3_10.java
+++ src/net/scriptsource/aufgaben/Aufgabe3_10.java
@@ -6,28 +6,30 @@ import java.util.Scanner;
 
 public class Aufgabe3_10 {
     public static void main(String[] args) {
-        String name, prename, date = "05.04.1976";
-        long tDiff = 0;
+        String name, prename, date;
+        long tDiff = 0, tAge = 0;
         Date dateNow = new Date();
         Date birthDay = new Date();
         SimpleDateFormat sDate = new SimpleDateFormat("dd.MM.yyyy");
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Wie ist ihr Nachname?");
         name=sc.next();
         System.out.println("Wie ist ihr Vorname?");
         prename=sc.next();
+
         System.out.println("Wann sind Sie geboren? [dd.mm.yyyy]");
         date = sc.next();
         try{        //Calculating the days of birthday until today
             birthDay =sDate.parse(date);
-            tDiff = ((dateNow.getTime()-birthDay.getTime())/(1000*60*60*24))%365;
+            tDiff = ((dateNow.getTime()-birthDay.getTime())/(1000*60*60*24));
         } catch (ParseException excep){
             excep.printStackTrace();
         }
+
         System.out.println("Hallo " + prename + " " + name +
                 ".\nHeute haben wir den " + sDate.format(dateNow) +
                 ".\nDu wurdest vor " + tDiff + " Tagen am "+
-                "\n" + sDate.format(birthDay) + " geboren!");
+                "\n" + sDate.format(birthDay) + " geboren!" +
+                "\nSomit bist du heute "+tAge +" Jahre alt");
     }
 }
