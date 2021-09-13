ae826ff5e1a7d235c87c23fc086fdd7a6731db38
Gion Desax <gion.desax@script-source.net>
2021-09-13 18:27:16 +0200

    Update gitignore and more

--- src/net/scriptsource/aufgaben/Aufgabe3_11.java
+++ src/net/scriptsource/aufgaben/Aufgabe3_11.java
@@ -4,13 +4,13 @@ public class Aufgabe3_11 {
     public static void main(String[] args) {
         int a = 1, b=2, c=3,d=4;
 
-        System.out.println(++a);            //1+1=2
-        System.out.println(a);
-        System.out.println(b++);            //
-        System.out.println(b);
-        System.out.println((++c)+(++c));
-        System.out.println(c);
-        System.out.println((d++)+(d++));
-        System.out.println(d);
+        System.out.println(++a);            // 2
+        System.out.println(a);              // 2
+        System.out.println(b++);            // 2 ist bereits schon auf 2 iteriert
+        System.out.println(b);              // 3 wird beim direkten Variablenaufruf iteriert
+        System.out.println((++c)+(++c));    // 4 + 5
+        System.out.println(c);              // 5 wird beim nächsten Variablenaufruf iteriert
+        System.out.println((d++)+(d++));    // 4 + 5
+        System.out.println(d);              // 6 wird weiter iteriert
     }
 }
