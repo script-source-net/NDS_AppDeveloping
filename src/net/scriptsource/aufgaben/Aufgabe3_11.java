3f79d972e25d1d3f35a36454a979d67e1e3b220c
Gion Desax <gion.desax@script-source.net>
2021-09-13 18:17:08 +0200

    Update gitignore and more

---
+++ src/net/scriptsource/aufgaben/Aufgabe3_11.java
@@ -0,0 +1,16 @@
+package net.scriptsource.aufgaben;
+
+public class Aufgabe3_11 {
+    public static void main(String[] args) {
+        int a = 1, b=2, c=3,d=4;
+
+        System.out.println(++a);            //1+1=2
+        System.out.println(a);
+        System.out.println(b++);            //
+        System.out.println(b);
+        System.out.println((++c)+(++c));
+        System.out.println(c);
+        System.out.println((d++)+(d++));
+        System.out.println(d);
+    }
+}
