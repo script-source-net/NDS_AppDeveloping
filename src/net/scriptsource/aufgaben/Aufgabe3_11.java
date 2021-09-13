daafafc1bf5327e15bc57ee5b2e9a02a6dc1f88a
Gion Desax <gion.desax@script-source.net>
2021-09-13 22:39:30 +0200

    Update gitignore and more

--- src/net/scriptsource/aufgaben/Aufgabe3_11.java
+++ src/net/scriptsource/aufgaben/Aufgabe3_11.java
@@ -6,11 +6,16 @@ public class Aufgabe3_11 {
 
         System.out.println(++a);            // 2
         System.out.println(a);              // 2
-        System.out.println(b++);            // 2 ist bereits schon auf 2 iteriert
-        System.out.println(b);              // 3 wird beim direkten Variablenaufruf iteriert
+        System.out.println(b++);            // 2
+        System.out.println(b);              // 3
         System.out.println((++c)+(++c));    // 4 + 5
-        System.out.println(c);              // 5 wird beim nächsten Variablenaufruf iteriert
+        System.out.println(c);              // 5
         System.out.println((d++)+(d++));    // 4 + 5
-        System.out.println(d);              // 6 wird weiter iteriert
+        System.out.println(d);              // 6
+        a=5;
+        System.out.println(++a*2);
+        a=5;
+        System.out.println(a++*2);
+        System.out.println(a);
     }
 }
---
+++ src/net/scriptsource/aufgaben/Aufgabe3_12.java
@@ -0,0 +1,20 @@
+package net.scriptsource.aufgaben;
+
+public class Aufgabe3_12 {
+    public static void main(String[] args) {
+        System.out.println("Dies " + "ist " + "ein " + "Concat String"); // Hier werden verschiedene Strings zusammen ausgegeben
+        System.out.println("2"+"3"); //Hiermit werden 2 und 3 zusammengenommen, als StringConcat
+        double x = 1.0, y = 2.5;
+        System.out.println(x/y);
+        System.out.println("x/y=" + x/y);
+        System.out.println(x+y);
+        System.out.println("x+y="+(x+y));
+        System.out.println(x-y);
+        System.out.println("x-y="+(x-y));
+        System.out.println(1+2+3+4);
+        System.out.println(1+2+3+"4");
+        System.out.println("1"+(2+3+4));
+        System.out.println("Hilfe" + true + 3);
+        System.out.println(true + (3 + "Hilfe"));
+    }
+}
