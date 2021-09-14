0b51f59219e1e13fc6ceb75c57fcafb99dc24e1b
Gion Desax <gion.desax@script-source.net>
2021-09-14 16:41:48 +0200

    Update Aufgabe3_15.java

---
+++ src/net/scriptsource/aufgaben/Aufgabe3_15.java
@@ -0,0 +1,12 @@
+package net.scriptsource.aufgaben;
+
+import java.util.Scanner;
+
+public class Aufgabe3_15 {
+    public static void main(String[] args) {
+        int t;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben sie eine Zahl über 1000 für die Berechnung ein:");
+        t= sc.nextInt();
+    }
+}
