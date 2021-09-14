cbb7e94427520179e569b03cd3ab38977d071cc2
Gion Desax <gion.desax@script-source.net>
2021-09-14 14:28:53 +0200

    Aufgabe3_14 Udate

--- src/net/scriptsource/aufgaben/Aufgabe3_13.java
+++ src/net/scriptsource/aufgaben/Aufgabe3_13.java
@@ -7,18 +7,22 @@ public class Aufgabe3_13 {
         x2=1223;
         x3=1e18;
         x4=1e15;
-        x5=3;
+        x5=3.0;
         x6=-1e12;
         y1=1e20;
-        y2=2;
+        y2=2.0;
         y3=-1e22;
         y4=1e13;
-        y5=2111;
+        y5=2111.0;
         y6=1e16;
         s = x1;
         System.out.println(s);
         s = x1*y1 + x2*y2 + x3*y3 + x4*y4 + x5*y5 + x6*y6;
         System.out.println("s = " + s);
+
+        double x=192119201, y=35675640;
+        s=(1.0/107751)*(1682*x*(y*y*y*y)+(3*(x*x*x)+29*x*(y*y)-2*(x*x*x*x*x)+832));
+        System.out.print("s = " +s);
     }
 
 }
