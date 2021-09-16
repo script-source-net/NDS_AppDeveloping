2fea81fea497be734dee0d8a00f04d11f4d18d9d
Gion Desax <gion.desax@script-source.net>
2021-09-16 11:09:06 +0200

    Update Aufgabe3_20.java

--- src/net/scriptsource/aufgaben/Aufgabe3_20.java
+++ src/net/scriptsource/aufgaben/Aufgabe3_20.java
@@ -1,12 +1,39 @@
 package net.scriptsource.aufgaben;
+/*
+Abfüllen eines mehrdimensionalen Arrays welches auf mehrere Reihen und Kollonen
+sich pro Reihe um 1 erhöhende Kollonenzahlen beinhaltet
 
+Beispiel:
+1 2 3 4
+2 3 4 5
+3 4 5 6
+ */
 public class Aufgabe3_20 {
     public static void main(String[] args) {
-        int a, b, c, d, e, f, g, h;
+        int startInner=1, endInner=9, indexRow = 0, indexColumn=0;
+        String output;
+        int[][] rows = new int[8][8];
+
+        //Abfüllen eine Mehrdimensionalen Arrays mit Zahlen
         for(int i=1;i<9;i++){
-            for (int j =1; j<9;j++){
-                System.out.print(j + " ");
+            indexColumn = 0;                        //Zurücksetzen des Kollonenindexes für das Arrays
+            for (int j=startInner;j<endInner;j++){  //Abfüllen der Kollone des Multidimensionalen Arrays
+                //System.out.print(j + " ");
+                rows[indexRow][indexColumn]+=j;     //Eintragen der Zahl auf dem Array bei Reihe und Kollonne
+                indexColumn++;
             }
+            startInner++;                           //Erhöhung der Startzahl für die Schleife
+            endInner++;                             //Erhöhung der Endzahl für die Schleife
+            indexRow++;                             //Erhöhung des Reihenindexes des Arrays
+            //System.out.println("");
+        }
+
+        for(int i=0; i<rows.length;i++){            //Schleife für den Output des Mehrdimensionlen Arrays
+            output="";
+            for(int j=0; j<rows[i].length;j++){
+                output += rows[i][j] + " ";
+            }
+            System.out.println(output);
         }
     }
 
