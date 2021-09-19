8cf58729ebe16dcbbf2c04642167ce6f29274356
Gion Desax <gion.desax@script-source.net>
2021-09-19 10:44:34 +0200

    Linux Update

--- src/net/scriptsource/aufgaben/Aufgabe3_20.java
+++ src/net/scriptsource/aufgaben/Aufgabe3_20.java
@@ -10,17 +10,16 @@ Beispiel:
  */
 public class Aufgabe3_20 {
     public static void main(String[] args) {
-        int startInner=1, endInner=9, indexRow = 0, indexColumn;
-        String output;
+        int startInner = 1, endInner = 9, indexRow = 0, indexColumn;
+        String output = "";
         int[][] rows = new int[8][8];
 
         //Abfüllen eine Mehrdimensionalen Arrays mit Zahlen
-        for(int i=1;i<9;i++){
-<<<<<<< Updated upstream
+        for (int i = 1; i < 9; i++) {
             indexColumn = 0;                        //Zurücksetzen des Kollonenindexes für das Arrays
-            for (int j=startInner;j<endInner;j++){  //Abfüllen der Kollone des Multidimensionalen Arrays
+            for (int j = startInner; j < endInner; j++) {  //Abfüllen der Kollone des Multidimensionalen Arrays
                 //System.out.print(j + " ");
-                rows[indexRow][indexColumn]+=j;     //Eintragen der Zahl auf dem Array bei Reihe und Kollonne
+                rows[indexRow][indexColumn] += j;     //Eintragen der Zahl auf dem Array bei Reihe und Kollonne
                 indexColumn++;
             }
             startInner++;                           //Erhöhung der Startzahl für die Schleife
@@ -29,26 +28,20 @@ public class Aufgabe3_20 {
             //System.out.println("");
         }
 
-        for(int i=0; i<rows.length;i++){            //Schleife für den Output des Mehrdimensionlen Arrays
-            output="";
-            for(int j=0; j<rows[i].length;j++){
+        for (int i = 0; i < rows.length; i++) {            //Schleife für den Output des Mehrdimensionlen Arrays
+            output = "";
+            for (int j = 0; j < rows[i].length; j++) {
                 output += rows[i][j] + " ";
             }
             System.out.println(output);
         }
         System.out.println("************************");
-        for (int[] innerArray:rows){
-            output="";
-            for(int data: innerArray){
-                output+=data + " ";
-=======
-            for (int j =1; j<9;j++){
-                System.out.print(j + " ");
-
->>>>>>> Stashed changes
+        for (int[] innerArray : rows) {
+            output = "";
+            for (int data : innerArray) {
+                output += data + " ";
             }
             System.out.println(output);
         }
     }
-
 }
